e3d5dda4709c36709ab2f896649f6c7b9d04c0d5
Josh <joshtesh91@mail.com>
2022-10-18 17:25:22 +0800

    add label class

--- item.rb
+++ item.rb
@@ -1,24 +1,36 @@
+# frozen_string_literal: true
+
 require 'date'
+
+# class for Item
 class Item
-    attr_accessor :genre, :author, :publisher
-    attr_reader :id, :published_date, :archived_at, :items
-    def initialize(id, published_date, archived_at)
-        @id = id
-        @published_date = published_date
-        @archived_at = archived_at 
-        @items = []
-    end
-    def can_be_archived?() 
-      @published_date.year < (Date.today - 10).year
-    end
-    def move_to_archive() 
-        return unless can_be_archived?
-    end
-    def add_author(author)
-        @items << author unless @items.include?(author)
-        
-    end
-    def add_genre(genre)
-        @genre = genre
-    end 
+  attr_accessor :genre, :author, :publisher
+  attr_reader :id, :published_date, :archived_at, :items
+
+  def initialize(id, published_date, archived_at)
+    @id = id
+    @published_date = published_date
+    @archived_at = archived_at
+    @items = []
+  end
+
+  def can_be_archived?
+    @published_date.year < (Date.today - 10).year
+  end
+
+  def move_to_archive
+    return unless can_be_archived?
+  end
+
+  def add_author(author)
+    @items << author unless @items.include?(author)
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_label(label)
+    @label = label
+  end
 end
