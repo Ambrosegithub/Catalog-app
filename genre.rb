ab15304736e81b9aa1303e9e216057a4aa10cf48
a-laarabi <contact.laarabi@gmail.com>
2022-10-18 12:21:08 +0100

    Update genre.rb

--- genre.rb
+++ genre.rb
@@ -2,9 +2,14 @@ class Genre
   attr_accessor :name, :items
   attr_reader :id
 
-  def initialize(id, name)
-    @id = id
+  def initialize(name)
+    @id = Random.rand(1..1000)
     @name = name
     @item = []
+  end
+
+  def add_item(item)
+    @item << item
+    item.genre = self
   end
 end
