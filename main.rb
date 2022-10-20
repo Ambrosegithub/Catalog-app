d887474bee34d779e2c190da76c22fd16ff58c66
a-laarabi <contact.laarabi@gmail.com>
2022-10-20 13:06:38 +0100

    Fix linters

--- main.rb
+++ main.rb
@@ -4,30 +4,28 @@ APP = App.new
 
 class Main
   def actions
-    puts "Welcome to Catalog of my things application"
-    puts "Please choose an option from the list by entering a number:"
-    puts "1 - Add book"
-    puts "2 - List all books"
-    puts "3 - Add a game"
-    puts "4 - List all games"
-    puts "5 - Add music album"
-    puts "6 - List all music albums"
-    puts "7 - List all genres"
-    puts ""
-    puts ""
-    puts ""
-    puts ""
-    puts ""
-    puts "0 - Exit"
+    puts 'Welcome to Catalog of my things application'
+    puts 'Please choose an option from the list by entering a number:'
+    puts '1 - Add book'
+    puts '2 - List all books'
+    puts '3 - Add a game'
+    puts '4 - List all games'
+    puts '5 - Add music album'
+    puts '6 - List all music albums'
+    puts '7 - List all genres'
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts ''
+    puts '15 - Exit'
   end
 
   def run
     actions
     action = gets.chomp.to_i
     choice(action)
-    if action == 0
-      exit
-    end
+    exit if action == 15
   end
 
   def choice(action)
